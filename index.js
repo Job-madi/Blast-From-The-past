7a34a28e242fc024f56d1fd5c3885d4673364ee0
joeyy <job.madi@protonmail.com>
2021-04-10 14:32:01 +0200

    seperate js index.html

---
+++ index.js
@@ -0,0 +1,23 @@
+  //Take out car image after 5 seconds
+setTimeout(function () {
+
+    document.getElementById("overlay").style.display = "none";
+
+}, 6000);  // 5 seconds
+
+
+//Following plays the song then stops it
+var myaud = document.getElementById("aud");
+var k = setInterval("pauseAud()", 6000);
+
+function playAud() {
+    myaud.play();
+}
+
+function pauseAud() {
+    myaud.pause();
+
+    clearInterval(k);
+}
+
+
